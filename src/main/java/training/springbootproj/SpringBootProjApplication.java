1b154e9daa278629f2d6207a6acf6923e949c386
Viktor <viktor.koevesdi@compusafe.de>
2024-03-19 09:46:39 +0100

    ToDo Entity added

--- src/main/java/training/springbootproj/SpringBootProjApplication.java
+++ src/main/java/training/springbootproj/SpringBootProjApplication.java
@@ -1,4 +1,4 @@
-package com.example.springbootproj;
+package training.springbootproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/training/springbootproj/SpringBootProjApplicationTests.java
+++ src/test/java/training/springbootproj/SpringBootProjApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springbootproj;
+package training.springbootproj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
