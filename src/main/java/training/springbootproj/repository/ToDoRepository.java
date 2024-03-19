433b65c6db3d88e8eafe1956e2dbf87ff370410c
Viktor <viktor.koevesdi@compusafe.de>
2024-03-19 11:44:09 +0100

    yml added

--- src/main/java/training/springbootproj/repository/ToDoRepository.java
+++ src/main/java/training/springbootproj/repository/ToDoRepository.java
@@ -8,39 +8,4 @@ import java.util.Optional;
 
 @Repository
 public interface ToDoRepository extends CrudRepository<ToDo, Long> {
-    @Override
-    <S extends ToDo> S save(S entity);
-
-    @Override
-    <S extends ToDo> Iterable<S> saveAll(Iterable<S> entities);
-
-    @Override
-    Optional<ToDo> findById(Long aLong);
-
-    @Override
-    boolean existsById(Long aLong);
-
-    @Override
-    Iterable<ToDo> findAll();
-
-    @Override
-    Iterable<ToDo> findAllById(Iterable<Long> longs);
-
-    @Override
-    long count();
-
-    @Override
-    void deleteById(Long aLong);
-
-    @Override
-    void delete(ToDo entity);
-
-    @Override
-    void deleteAllById(Iterable<? extends Long> longs);
-
-    @Override
-    void deleteAll(Iterable<? extends ToDo> entities);
-
-    @Override
-    void deleteAll();
 }
